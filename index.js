d02f7c9754a07865bc3b0e3400e819543b0c584a
ryan-dutton <ryan.dutton@gmail.com>
2023-08-28 18:43:23 +0800

    add route for multipart upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const sharp = require('sharp');
 const express = require('express');
+const fileUpload = require('express-fileupload');
+
 const https = require('https')
 const app = express();
 const port = 443;
@@ -28,7 +30,7 @@ let test_form = `
 			let w = document.getElementById('width').value;
 			let h = document.getElementById('height').value;
 			let k = document.getElementById('key').value;
-			let f = document.querySelector('input[name="format"]:checked').value;
+			let f = document.querySelector('input[id="format"]:checked').value;
 			let body = JSON.stringify({u,w,h,k,f});
 			console.log(body);
 			try {
@@ -50,6 +52,7 @@ let test_form = `
 			}
 		}
 	</script>
+	<h1>Remote URL</h1>
 	<table border=1>
 		<tr>
 			<td>URL</td>
@@ -70,24 +73,56 @@ let test_form = `
 		<tr>
 			<td>Format</td>
 			<td>
-				<input selected type=radio name="format" value="png"/>PNG
-				<input type=radio name="format" value="jpeg"/>JPEG
-				<input type=radio name="format" value="gif"/>GIF
+				<input selected type=radio id="format" value="png"/>PNG
+				<input type=radio id="format" value="jpeg"/>JPEG
+				<input type=radio id="format" value="gif"/>GIF
 			</td> 
 		</tr>
 	</table>
 	<button type=button onclick="test();">Test</button><br/>
 	<img src="" id="output"/>
+	<hr/>
+	<h1>File Upload</h1>
+	<form action="/upload" method="POST" enctype="multipart/form-data">
+		<table border=1>
+			<tr>
+				<td>File</td>
+				<td><input type="file" name="i" /></td>
+			</tr>
+			<tr>
+				<td>Width</td>
+				<td><input size="5" type=text name="w" value="800"/></td>
+			</tr>
+			<tr>
+				<td>Height</td>
+				<td><input size="5" type=text name="h" value="600"/></td>
+			</tr>
+			<tr>
+				<td>Key</td>
+				<td><input size="50" type=text name="k" value=""/></td>
+			</tr>
+			<tr>
+				<td>Format</td>
+				<td>
+					<input selected type=radio name="f" value="png"/>PNG
+					<input type=radio name="f" value="jpeg"/>JPEG
+					<input type=radio name="f" value="gif"/>GIF
+				</td> 
+			</tr>
+		</table>
+    	<button type="submit">Upload</button>
+	</form>
 </html>
 `;
 
-app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
 	res.send(test_form)
 })
 
-app.post('/', async (req, res) => {
+app.post('/', express.json(), async (req, res) => {
 	try {
 		if (!(req.body.k && req.body.k === process.env.KEY)) {
 			res.status(401);
@@ -116,6 +151,36 @@ app.post('/', async (req, res) => {
 	} catch (e) {
 		res.status(400);
 		res.send(e.message);
+		console.log("processing failed with message:", e.message);
+		return;
+	}
+})
+
+app.post('/upload', express.urlencoded(), fileUpload(), async (req, res) => {
+	try {
+		if (!(req.body.k && req.body.k === process.env.KEY)) {
+			res.status(401);
+			res.send("Invalid key specified."+req.body.k);
+			return;
+		}
+		let { i } = req.files;
+		
+		let format = req.body.f && ['jpeg','gif','png'].includes(req.body.f) ? req.body.f : default_output_format;
+		let new_image_buffer =
+			await sharp(i.data)
+			.resize(
+				parseInt(req.body.w),
+				parseInt(req.body.h)
+			)
+			[format](default_output_format_options[format])
+			.toBuffer();
+		res.status(200);
+		res.setHeader('content-type', 'image/'+format);
+		res.send(new_image_buffer);
+		return;
+	} catch (e) {
+		res.status(400);
+		res.send("Error:"+e.message);
 		console.log("processing failed with message:", e.message);
 		return;
 	}
